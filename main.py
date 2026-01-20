deedf59bb4894d8d69cae7d88f7c07111847c8ac
tristan.coulaud <tristan.coulaud@hotmail.fr>
2026-01-20 14:06:07 +0100

    Create Frontend

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.tickets import router as tickets_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(tickets_router)
 
